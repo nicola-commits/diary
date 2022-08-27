1ebd5affb31b718420add7c9f34d1af84a6df1cd
unknown <nicolamigone179@gmail.com>
2022-08-27 15:00:44 +0200

    performance improved, on new_page: title text created, fixed positioning on body text

--- app/src/main/java/com/example/diary/MainActivity.kt
+++ app/src/main/java/com/example/diary/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.diary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,13 @@ class MainActivity : AppCompatActivity() {
             val readDiaryButton : Button = findViewById(R.id.readDiaryButton)
             readDiaryButton.setOnClickListener {
                 setContentView(R.layout.read_diary)
+                //set the title of the diary page to the device's date
+                val title : TextView = findViewById(R.id.editTextTitle)
+                val c = Calendar.getInstance()
+                val year = c.get(Calendar.YEAR)
+                val month = c.get(Calendar.MONTH)
+                val day = c.get(Calendar.DAY_OF_MONTH)
+                title.text = "$year/$month/$day"
             }
         }
         catch(e: Error){
