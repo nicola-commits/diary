a9c05036a35484edb8c3161d658b5ed64c615a00
unknown <nicolamigone179@gmail.com>
2022-08-29 11:44:26 +0200

    Save and Write functionality added and working, finished up NewPage

--- app/src/main/java/com/example/diary/NewPage.kt
+++ app/src/main/java/com/example/diary/NewPage.kt
@@ -3,14 +3,10 @@ package com.example.diary
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Base64
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONObject
-import java.io.*
-import java.security.InvalidParameterException
 
 class NewPage: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,22 +20,43 @@ class NewPage: AppCompatActivity() {
             //save all the work
             val noteTitle = findViewById<TextView>(R.id.editTextTitle).text
             val noteBody = findViewById<TextView>(R.id.editTextTextMultiLine).text
+            val delimiter = getString(R.string.list_delimiter)
 
-            if(noteTitle != null && noteTitle != "") {
+            if(noteTitle == null || noteTitle == "" || noteTitle.contains(delimiter)) {
+
+                Toast.makeText(this, "Invalid title! Title can't be empty or contain $delimiter!", Toast.LENGTH_LONG).show()
+
+            } else{
 
                 val sp = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
-                sp.edit().apply{
-                    putString(noteTitle.toString(), noteBody.toString())
-                }.apply()
+                val editor = sp.edit()
+                //get current notes, then add to them if the current one isn't there
+                var notes = sp.getString("allNotes", null)
+                editor.remove("allNotes") //so that it can be overwritten
+
+                if(notes == null){
+                    //in this case the note we want to save is the first note of the user, so we can just do it "manually"
+                    editor.apply{
+                        putString("allNotes", noteTitle.toString())
+                        putString(noteTitle.toString(), noteBody.toString())
+                    }.apply()
+
+                } else{
+                    //the user has already written at least another note
+                    //add the delimiter and the name of the note at the end
+                    notes = notes + delimiter + noteTitle
+
+                    editor.apply{
+                        putString("allNotes", notes)
+                        putString(noteTitle.toString(), noteBody.toString())
+                    }.apply()
+                }
 
                 Toast.makeText(this, "Data saved!", Toast.LENGTH_LONG).show()
 
-                val i: Intent = Intent(this@NewPage, MainActivity::class.java)
+                val i = Intent(this@NewPage, MainActivity::class.java)
                 startActivity(i)
-            }
 
-            else{
-                Toast.makeText(this, "Invalid title", Toast.LENGTH_LONG).show()
             }
         }
     }
--- app/src/main/java/com/example/diary/ReadDiary.kt
+++ app/src/main/java/com/example/diary/ReadDiary.kt
@@ -1,11 +1,34 @@
 package com.example.diary
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ReadDiary: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.read_diary)
+
+        val txt : TextView = findViewById(R.id.AllNotesTextView)
+        //set backHome button
+        val bhb : Button = findViewById(R.id.backHomeButtonRead)
+        bhb.setOnClickListener {
+            val i = Intent(this@ReadDiary, MainActivity::class.java)
+            startActivity(i)
+        }
+
+        //read saved files
+        val sp = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+        val notes = sp.getString("allNotes", null)
+
+        txt.text = notes.toString()
+
+
+        Toast.makeText(this, notes, Toast.LENGTH_LONG).show()
+
+
     }
 }
