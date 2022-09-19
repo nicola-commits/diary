59e300791e9e025a7bc8232661b64c081a10d5f0
unknown <nicolamigone179@gmail.com>
2022-09-19 22:42:37 +0200

    Created primitive User class

--- app/src/main/java/com/example/diary/User.kt
+++ app/src/main/java/com/example/diary/User.kt
@@ -1,4 +1,27 @@
 package com.example.diary
 
+import android.content.Context
+import com.google.gson.Gson
+
 class User {
+    private var data:HashMap<String, String?>? = hashMapOf("name" to null, "notes" to null, "theme" to "light", "language" to "english")
+    private var notes:Map<String, Map<String, String?>>? = hashMapOf()
+
+    fun getData(name: String): String? {
+        return this.data!![name]
+    }
+
+    fun setData(name:String, value:String?) {
+        this.data!![name] = value
+    }
+
+    fun saveData(c: Context){
+        val sp = c.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val editor = sp.edit()
+        val gson = Gson()
+        val json = gson.toJson(this)
+        editor.putString("MyObject", json)
+        editor.apply()
+    }
+
 }
